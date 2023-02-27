59aa7005dd7e5cad3008af519011c6ec0a059b88
GabrielMSk8 <cafepreto97@gmail.com>
2023-02-27 15:50:39 -0300

    Exercício introdutório ao React

---
+++ src/components/home/Home.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+        <body className='body'>
+            
+        
+        <h1>Skatista</h1>
+        <p className='texto'>lorem ipsumlorem ipsumlorem 
+        ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem
+         ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum
+        </p>
+        <img className='img' src="https://th.bing.com/th/id/R.fab1ff4d19b598d87e77eeff88f3c31e?rik=uCUhh5FZD%2bwP2g&riu=http%3a%2f%2fbigparksproject.org.uk%2fwp-content%2fuploads%2f2013%2f11%2f16556096888_b11451bdab_o-1024x836.jpg&ehk=BqhQ5%2bu16xEHsPys86zB6lTRIPt7rVpOeEPDDFP0ZB8%3d&risl=&pid=ImgRaw&r=0" alt="foto" />
+        </body>
+    </div>
+  );
+}
+
+export default Home;
